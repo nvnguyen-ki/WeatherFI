6329e36f3df4ce26247871f7200f02f4830cba59
LAPTOP-J906GN70\Nguyen <nguyentwotimes@gmail.com>
2019-05-24 19:46:50 -0700

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -16,7 +16,7 @@ messageTwo.textContent = ''
 weatherForm.addEventListener('submit', (e) => {
   e.preventDefault()
   const location = search.value
-  fetch('http://localhost:3000/weather?address='+location).then((response,error) => {
+  fetch( '/weather?address='+location).then((response,error) => {
 
     response.json().then((data) => {
       if (data.error) {
