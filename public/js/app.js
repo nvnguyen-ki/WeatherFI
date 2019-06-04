e3ec1565c89925d35f6dbebe5791ee55358320e3
LAPTOP-J906GN70\Nguyen <nguyentwotimes@gmail.com>
2019-06-04 13:19:06 -0700

    nav bar beginning

--- public/js/app.js
+++ public/js/app.js
@@ -24,35 +24,35 @@ weatherForm.addEventListener('submit', (e) => {
       highlow.textContent = data.forecast.highlow
       // changes atmosphere depending on weather at location
       if(data.forecast.singleData == 'wind') {
-        document.querySelector(".main-content").style.background = 'url(https://i.pinimg.com/originals/ac/f3/0e/acf30ee54206580bca0cb291eab559f4.gif)';
-        document.querySelector(".main-content").style.backgroundSize = 'cover';
-        document.querySelector(".main-content").style.backgroundPosition = 'center';
+        document.querySelector("#weather-page").style.background = 'linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),url(https://i.pinimg.com/originals/ac/f3/0e/acf30ee54206580bca0cb291eab559f4.gif)';
+        document.querySelector("#weather-page").style.backgroundSize = 'cover';
+        document.querySelector("#weather-page").style.backgroundPosition = 'center';
         document.getElementById("songsrc").src = 'audio/windy.mp3';
       }
       if(data.forecast.singleData == 'clear-day' || data.forecast.singleData == 'clear-night') {
-        document.querySelector(".main-content").style.background = 'url(https://data.whicdn.com/images/140771342/original.gif)';
-        document.querySelector(".main-content").style.backgroundSize = 'cover';
-        document.querySelector(".main-content").style.backgroundPosition = 'center';
+        document.querySelector("#weather-page").style.background = 'linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),url(https://data.whicdn.com/images/140771342/original.gif)';
+        document.querySelector("#weather-page").style.backgroundSize = 'cover';
+        document.querySelector("#weather-page").style.backgroundPosition = 'center';
         document.getElementById("songsrc").src = 'audio/true-sunny.mp3';
       }
       if(data.forecast.singleData == 'rain') {
-        document.querySelector(".main-content").style.background = 'url(https://data.whicdn.com/images/281328506/original.gif)';
-        document.querySelector(".main-content").style.backgroundSize = 'cover';
-        document.querySelector(".main-content").style.backgroundPosition = 'center';
+        document.querySelector("#weather-page").style.background = 'linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),url(https://data.whicdn.com/images/281328506/original.gif)';
+        document.querySelector("#weather-page").style.backgroundSize = 'cover';
+        document.querySelector("#weather-page").style.backgroundPosition = 'center';
         document.getElementById("songsrc").src = 'audio/lofiloop.mp3';
       }
 
       if(data.forecast.singleData == 'cloudy' || data.forecast.singleData == 'partly-cloudy-day' || data.forecast.singleData == 'partly-cloudy-night') {
-        document.querySelector(".main-content").style.background = 'url(https://media.giphy.com/media/lnq52t8atIw3m/giphy.gif)';
-        document.querySelector(".main-content").style.backgroundSize = 'cover';
-        document.querySelector(".main-content").style.backgroundPosition = 'center';
+        document.querySelector("#weather-page").style.background = 'linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),url(https://media.giphy.com/media/lnq52t8atIw3m/giphy.gif)';
+        document.querySelector("#weather-page").style.backgroundSize = 'cover';
+        document.querySelector("#weather-page").style.backgroundPosition = 'center';
         document.getElementById("songsrc").src = 'audio/sunny-song.mp3';
         
       }
       if(data.forecast.singleData == 'snow') {
-        document.querySelector(".main-content").style.background = 'url(https://media.giphy.com/media/Le0YdjvkL1bOg/giphy.gif)';
-        document.querySelector(".main-content").style.backgroundSize = 'cover';
-        document.querySelector(".main-content").style.backgroundPosition = 'center';
+        document.querySelector("#weather-page").style.background = 'linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),url(https://media.giphy.com/media/Le0YdjvkL1bOg/giphy.gif)';
+        document.querySelector("#weather-page").style.backgroundSize = 'cover';
+        document.querySelector("#weather-page").style.backgroundPosition = 'center';
         document.getElementById("songsrc").src = 'audio/snow2.mp3';
       }
       }
